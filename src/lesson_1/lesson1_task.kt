d1e0b98a463b7f8276e3ee3d56c1d987f0bef7f8
Kweyzzz <amal.5555@mail.ru>
2024-11-11 19:52:42 +0300

    KS-1-1 create smth

--- src/lesson_1/lesson1_task.kt
+++ src/lesson_1/lesson1_task.kt
@@ -1,5 +1,6 @@
 package lesson_1
 
 fun main(){
-
+    var orderCount: Int = 10
+    var customerAppreciationText: String = "Спасибо за вашу покупку! Мы ценим ваш выбор и рады обслужить вас. Ждём вас снова!"
 }

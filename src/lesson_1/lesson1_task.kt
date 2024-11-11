c6c89e8f1b978f652f1f0844564908b62d50b338
Kweyzzz <amal.5555@mail.ru>
2024-11-11 19:55:39 +0300

    KS-1-1 first task

--- src/lesson_1/lesson1_task.kt
+++ src/lesson_1/lesson1_task.kt
@@ -1,6 +1,8 @@
 package lesson_1
 
 fun main(){
-    var orderCount: Int = 10
-    var customerAppreciationText: String = "Спасибо за вашу покупку! Мы ценим ваш выбор и рады обслужить вас. Ждём вас снова!"
+    val orderCount: Int = 166
+    val customerAppreciationText: String = "Спасибо за вашу покупку! Мы ценим ваш выбор и рады обслужить вас. Ждём вас снова!"
+    println(orderCount)
+    println(customerAppreciationText)
 }

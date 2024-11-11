d89e7353695d9d78f79fc1cb903205a24ab4c98f
Kweyzzz <amal.5555@mail.ru>
2024-11-11 19:26:49 +0300

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val numberOfOrders = 42
+
+    var notificationText: String = "Не забудь маску и перчатки"
+
+    println(numberOfOrders)
+    println(notificationText)
+
+    notificationText = "Маски больше не потребуются"
+    println(notificationText)
+
+}
